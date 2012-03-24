41c64e6e4adb78173316a0092cfd2d015a506da6
s20307 <s20307@naver.com>
2012-03-24 15:29:09 +0900

    update HDM

--- Ex/ex.js
+++ Ex/ex.js
@@ -131,3 +131,52 @@ function showAllCards(cards) {
 		showCard(i, "background.jpg", fadeOutSpeed, fadeInSpeed);
 	}
 }
+
+function bindGetScoreBtn(){
+	$('#scoreBtn').click(function(){
+		getScoreList(appendScoreList);
+	});
+}
+
+
+function getScoreList(appendScoreList){
+	var url = '/game/card/findScore';
+	$.get(url, function(resultSet){
+		
+		var scoreObjList = [];
+		for ( var i = 0, max = resultSet.length; i < max; i ++){
+			var scoreObj = {};
+			scoreObj.userName = resultSet[i].userName; 
+			scoreObj.clearTime = resultSet[i].clearTime;
+			scoreObj.regDate = new Date(resultSet[i].regDate).toLocaleString();
+			
+			scoreObjList.push(scoreObj);
+			
+			appendScoreList(scoreObjList);
+		}
+	});
+}
+
+function appendScoreList(scoreObjList){
+	console.dir(scoreObjList);
+	// HTML TABLE 에 데이터 뿌리기 결과 있는경우 와 없는 경우 고려 ex) scoreObjList.length 체크
+}
+
+
+////////////////////////////////////////////////
+// 게임종료후 이름 / 클리어시간 DB저장 호출 함수
+////////////////////////////////////////////////
+function saveScore(userName, clearTime){
+	var url = '/game/card/findScore';
+	var param = {};
+	param.userName = userName;
+	param.clearTime = clearTime;
+	
+	$.get(url, param, function(result){
+		if ( result ){
+			alert('저장되었습니다.');
+		}else{
+			alert('저장을 실패하였습니다.');
+		}
+	});
+}
