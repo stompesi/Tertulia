8c14d1cbd4bea22e74abffb8173f0425f15041e0
unknown <computer@.(none)>
2012-03-31 15:31:49 +0900

    update

--- public/scripts/cardGame/cardGame.js
+++ public/scripts/cardGame/cardGame.js
@@ -1,23 +1,23 @@
 /*
- * 문서가 로드되면 순위보기 버튼에 대해서 바인딩 한다.
- * 순위보기 버튼이 클릭되면 서버에서 게임클리어 시간을 오름차순으로 정렬한 리스트를 반환한다.
- * 반환된 리스트를 가지고 TABLE UI를 꾸미도록 한다.
- */
+* 문서가 로드되면 순위보기 버튼에 대해서 바인딩 한다.
+* 순위보기 버튼이 클릭되면 서버에서 게임클리어 시간을 오름차순으로 정렬한 리스트를 반환한다.
+* 반환된 리스트를 가지고 TABLE UI를 꾸미도록 한다.
+*/
 
 //alert($('input#selectMapSize').val());
 //tr의 아이디를 정함
 
-$(document).ready(function(){
-	$('#start').bind('click',start);
+$(document).ready(function() {
+	$('#start').bind('click', start);
 	bindGetScoreBtn();
 });
-
 var mapSize;
 var IMG_PATH = "/images/cardGame/";
 
 function start() {
 
 	//게임맵을 그림
+
 	drawMap();
 
 	var check = {
@@ -32,11 +32,11 @@ function start() {
 	//카드전체 보여주기
 	showAllCards(cards);
 
+	var startTime = new Date().getDate();
 	//카드 아이디 보여주기 (삭제예정)
 	for(var i = 1; i <= mapSize; i++) {
 		console.log(cards[i].id);
 	}
-
 	$('img').click(function() {
 		var sequence = this.id;
 		var fadeInSpeed = 300;
@@ -44,7 +44,7 @@ function start() {
 		var delaySpeed = 500;
 		if(cards[sequence].isEvert) {
 			cards[sequence].isEvert = false;
-			showCard(sequence, IMG_PATH+cards[sequence].id + ".png", fadeOutSpeed, fadeInSpeed);
+			showCard(sequence, IMG_PATH + cards[sequence].id + ".png", fadeOutSpeed, fadeInSpeed);
 			if(check.id == "") {
 				check.id = cards[sequence].id;
 				check.sequence = cards[sequence].sequence;
@@ -52,10 +52,10 @@ function start() {
 				$('#' + check.sequence).delay(delaySpeed);
 				if(check.id == cards[sequence].id) {
 					check.id = "";
-					checkFinishGame(cards);
+					checkFinishGame(cards, startTime);
 				} else {
-					showCard(cards[sequence].sequence, IMG_PATH+"background.png", fadeOutSpeed, fadeInSpeed);
-					showCard(check.sequence, IMG_PATH+"background.png", fadeOutSpeed, fadeInSpeed);
+					showCard(cards[sequence].sequence, IMG_PATH + "background.png", fadeOutSpeed, fadeInSpeed);
+					showCard(check.sequence, IMG_PATH + "background.png", fadeOutSpeed, fadeInSpeed);
 					cards[sequence].isEvert = true;
 					cards[check.sequence].isEvert = true;
 					check.id = "";
@@ -73,10 +73,11 @@ function drawMap() {
 			var trId = $('<tr/>');
 			$('#gameMap').append(trId);
 		}
-		$(trId).append('<td><img class="cardImg"id=' + i + ' src="'+IMG_PATH+'background.png"/></td>');
+		$(trId).append('<td><img class="cardImg"id=' + i + ' src="' + IMG_PATH + 'background.png"/></td>');
 	}
-	cardImgSizeChange(mapSize);	
+	cardImgSizeChange(mapSize);
 }
+
 function makeCards(cards) {
 	var randomId = [];
 	for(var i = 1; i <= mapSize; i++) {
@@ -86,24 +87,25 @@ function makeCards(cards) {
 			isEvert : true
 		};
 	}
-	for(var i=1; i<=mapSize/2; i++){
+	for(var i = 1; i <= mapSize / 2; i++) {
 
-		randomId[i]= random(283);
-		for(var j = 1 ; j<= mapSize/2 ; j++){
-			if(i!=j && randomId[i]==randomId[j] ){
+		randomId[i] = random(283);
+		for(var j = 1; j <= mapSize / 2; j++) {
+			if(i != j && randomId[i] == randomId[j]) {
 				i--;
 				break;
 			}
 		}
 	}
-	for(var i = 1 ,j=0; i <= mapSize; i++) {
+	for(var i = 1, j = 0; i <= mapSize; i++) {
 		var ransequence = random(mapSize);
 
 		if(cards[ransequence].id == "") {
-			if(i%2==1){
+			if(i % 2 == 1) {
 				j++;
 			}
-			cards[ransequence].id = randomId[j] ;//i % (Math.pow(mapSize, 0.5) * 2) + 1;
+			cards[ransequence].id = randomId[j];
+			//i % (Math.pow(mapSize, 0.5) * 2) + 1;
 		} else {
 			i--;
 		}
@@ -121,11 +123,12 @@ function random(randomSize) {
 	return Math.ceil(Math.random() * randomSize);
 }
 
-function checkFinishGame(cards) {
+function checkFinishGame(cards, startTime) {
 	for(var i = 1; i <= mapSize; i++) {
 		if(cards[i].isEvert == false) {
-			if(i == mapSize){
-				alert('게임을 완료하였습니다.');
+			if(i == mapSize) {
+				var clearTime = clearGameTime(startTime);
+				saveResult(clearTime);
 			}
 		} else {
 			break;
@@ -133,58 +136,61 @@ function checkFinishGame(cards) {
 	}
 }
 
+function clearGameTime(startTime) {
+	return (new Date().getDate() ) - startTime / 1000;
+}
+
+function saveResult(clearTime) {
+	var userName = prompt("게임을 완료하였습니다. 아이디를 입력하여 주세요.");
+//	alert(clearTime);
+	var url = '/game/card/saveScore';
+	var param = {};
+	param.userName = userName;
+	param.clearTime = clearTime;
+
+	$.get(url, param, function(result) {
+		if(result) {
+			alert('저장되었습니다.');
+		} else {
+			alert('저장을 실패하였습니다.');
+		}
+	});
+}
+
 function showAllCards(cards) {
 	var fadeInSpeed = 1000;
 	var fadeOutSpeed = 500;
 	for(var i = 1; i <= mapSize; i++) {
-		showCard(i, IMG_PATH+cards[i].id + ".png", fadeOutSpeed, fadeInSpeed);
-		showCard(i,IMG_PATH+"background.png", fadeOutSpeed, fadeInSpeed);
+		showCard(i, IMG_PATH + cards[i].id + ".png", fadeOutSpeed, fadeInSpeed);
+		showCard(i, IMG_PATH + "background.png", fadeOutSpeed, fadeInSpeed);
 	}
 }
 
-function bindGetScoreBtn(){
-	$('#scoreBtn').click(function(){
+function bindGetScoreBtn() {
+	$('#scoreBtn').click(function() {
 		getScoreList(appendScoreList);
 	});
 }
 
-function getScoreList(appendScoreList){
+function getScoreList(appendScoreList) {
 	var url = '/game/card/findScore';
-	$.get(url, function(resultSet){
-		
+	$.get(url, function(resultSet) {
+
 		var scoreObjList = [];
-		for ( var i = 0, max = resultSet.length; i < max; i ++){
+		for(var i = 0, max = resultSet.length; i < max; i++) {
 			var scoreObj = {};
-			scoreObj.userName = resultSet[i].userName; 
+			scoreObj.userName = resultSet[i].userName;
 			scoreObj.clearTime = resultSet[i].clearTime;
 			scoreObj.regDate = new Date(resultSet[i].regDate).toLocaleString();
-			
+
 			scoreObjList.push(scoreObj);
-			
+
 			appendScoreList(scoreObjList);
 		}
 	});
 }
 
-function appendScoreList(scoreObjList){
+function appendScoreList(scoreObjList) {
 	console.dir(scoreObjList);
 	// HTML TABLE 에 데이터 뿌리기 결과 있는경우 와 없는 경우 고려 ex) scoreObjList.length 체크
-}
-
-////////////////////////////////////////////////
-// 게임종료후 이름 / 클리어시간 DB저장 호출 함수
-////////////////////////////////////////////////
-function saveScore(userName, clearTime){
-	var url = '/game/card/findScore';
-	var param = {};
-	param.userName = userName;
-	param.clearTime = clearTime;
-	
-	$.get(url, param, function(result){
-		if ( result ){
-			alert('저장되었습니다.');
-		}else{
-			alert('저장을 실패하였습니다.');
-		}
-	});
 }
